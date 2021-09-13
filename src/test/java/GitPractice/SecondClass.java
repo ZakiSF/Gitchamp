17a73956794ea47a7dac6a9d5a5dc2fd739608bd
ZakiSF <58540617+ZakiSF@users.noreply.github.com>
2021-09-13 11:51:00 -0700

    Update SecondClass.java

--- src/test/java/GitPractice/SecondClass.java
+++ src/test/java/GitPractice/SecondClass.java
@@ -5,5 +5,6 @@ public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Second Class change ");
         System.out.println("newchange");
+        System.out.println("55555");
     }
 }
